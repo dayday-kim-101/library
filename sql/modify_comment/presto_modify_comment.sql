a9f5214abec0f5a1b6032aac4ff6366f1f498ddd
dayday-kim-101 <daeheekim1027@gmail.com>
2021-04-20 15:47:21 +0900

    ADD: sql logic of modify table's column comment

---
+++ sql/modify_comment/presto_modify_comment.sql
@@ -0,0 +1,1 @@
+ALTER TABLE temp.dayday_test_del_column ADD COLUMN ord_dt VARCHAR COMMENT conmment
---
+++ sql/modify_comment/spark_modify_comment.sql
@@ -0,0 +1,1 @@
+ALTER TABLE temp.dayday_test_del_column ALTER COLUMN ord_dt COMMENT "new comment";
